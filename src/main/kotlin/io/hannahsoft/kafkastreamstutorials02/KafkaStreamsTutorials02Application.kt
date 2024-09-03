cb9de5f0f4ca4e323c8d4c5dfc8a90d8c077696d
Asanobm <asanobm@outlook.com>
2024-09-03 10:53:16 +0900

    create kafka streams with kotlin example 02

--- src/main/kotlin/io/hannahsoft/kafkastreamstutorials02/KafkaStreamsTutorials02Application.kt
+++ src/main/kotlin/io/hannahsoft/kafkastreamstutorials02/KafkaStreamsTutorials02Application.kt
@@ -1,11 +1,12 @@
 package io.hannahsoft.kafkastreamstutorials02
 
-import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.runApplication
+//import org.springframework.boot.autoconfigure.SpringBootApplication
+//import org.springframework.boot.runApplication
 
-@SpringBootApplication
+//@SpringBootApplication
 class KafkaStreamsTutorials02Application
 
 fun main(args: Array<String>) {
-  runApplication<KafkaStreamsTutorials02Application>(*args)
+//  runApplication<KafkaStreamsTutorials02Application>(*args)
 }
+cdg
