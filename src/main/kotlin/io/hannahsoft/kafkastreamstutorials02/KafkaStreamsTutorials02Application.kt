12eaef9a40b3ec922d5198df97c9f814b3886f62
Asanobm <asanobm@outlook.com>
2024-09-03 10:51:20 +0900

    create tutorial project

---
+++ src/main/kotlin/io/hannahsoft/kafkastreamstutorials02/KafkaStreamsTutorials02Application.kt
@@ -0,0 +1,11 @@
+package io.hannahsoft.kafkastreamstutorials02
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KafkaStreamsTutorials02Application
+
+fun main(args: Array<String>) {
+  runApplication<KafkaStreamsTutorials02Application>(*args)
+}
---
+++ src/test/kotlin/io/hannahsoft/kafkastreamstutorials02/KafkaStreamsTutorials02ApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.hannahsoft.kafkastreamstutorials02
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KafkaStreamsTutorials02ApplicationTests {
+
+  @Test
+  fun contextLoads() {
+  }
+
+}
